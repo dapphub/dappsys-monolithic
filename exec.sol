86bb7bf00cca5a258b8085f7be1c3bb9ef81c8cf
rain <rainbreak@riseup.net>
2017-08-07 15:19:45 +0100

    add ds-exec

---
+++ exec.sol
@@ -0,0 +1,59 @@
+/*
+   Copyright 2016-2017 DappHub, LLC
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// A base contract used by anything that wants to do "untyped" calls
+
+pragma solidity ^0.4.8;
+
+contract DSExec {
+    function tryExec( address target, bytes calldata, uint value)
+             internal
+             returns (bool call_ret)
+    {
+        return target.call.value(value)(calldata);
+    }
+    function exec( address target, bytes calldata, uint value)
+             internal
+    {
+        if(!tryExec(target, calldata, value)) {
+            throw;
+        }
+    }
+
+    // Convenience aliases
+    function exec( address t, bytes c )
+        internal
+    {
+        exec(t, c, 0);
+    }
+    function exec( address t, uint256 v )
+        internal
+    {
+        bytes memory c; exec(t, c, v);
+    }
+    function tryExec( address t, bytes c )
+        internal
+        returns (bool)
+    {
+        return tryExec(t, c, 0);
+    }
+    function tryExec( address t, uint256 v )
+        internal
+        returns (bool)
+    {
+        bytes memory c; return tryExec(t, c, v);
+    }
+}
