00429a4a7f66c25b58209c42abb119832e2e6c30
rain <rainbreak@riseup.net>
2017-12-07 07:11:51 +0000

    upgrade dappsys

--- thing.sol
+++ thing.sol
@@ -22,4 +22,9 @@ import "./note.sol";
 import "./math.sol";
 
 contract DSThing is DSAuth, DSNote, DSMath {
+
+    function S(string s) internal pure returns (bytes4) {
+        return bytes4(keccak256(s));
+    }
+
 }
